2cd780a557fb9b4f9dc599a35c986e17af1ab947
AlexisHuin <redemption41@gmail.com>
2023-04-26 10:22:00 +0200

    probléme transition objet vers BDD

--- public/app.js
+++ public/app.js
@@ -1,178 +1,185 @@
-const jeuxZelda = [
-  {
-    titre: "The Legend of Zelda",
-    annee: 1986,
-    description:
-      "Le premier jeu de la série Zelda, où les joueurs contrôlent Link pour sauver la princesse Zelda et vaincre le maléfique Ganon.",
-    console: "NES",
-    photo:
-      "https://upload.wikimedia.org/wikipedia/en/4/41/Legend_of_zelda_cover_%28with_cartridge%29_gold.png",
-  },
-  {
-    titre: "Zelda II: The Adventure of Link",
-    annee: 1987,
-    description:
-      "Un jeu de rôle d'action dans lequel Link doit trouver les trois reliques pour réveiller la princesse Zelda.",
-    console: "NES",
-    photo:
-      "https://fr.shopping.rakuten.com/photo/Zelda-Ii-The-Adventure-Of-Link-Jeu-Nintendo-Nes-957925970_L.jpg",
-  },
-  {
-    titre: "A Link to the Past",
-    annee: 1991,
-    description:
-      "Considéré comme l'un des meilleurs jeux de la série, A Link to the Past permet aux joueurs de voyager entre le monde de la lumière et le monde des ténèbres pour sauver Hyrule.",
-    console: "SNES",
-    photo:
-      "https://image.jeuxvideo.com/images/sn/l/o/lozpsn0f.jpg",
-  },
-  {
-    titre: "Link's Awakening",
-    annee: 1993,
-    description:
-      "Le premier jeu portable de la série Zelda, où Link doit récupérer les instruments pour réveiller le poisson-rêve.",
-    console: "Game Boy",
-    photo:
-      "https://i.etsystatic.com/16695346/r/il/92514e/2003836467/il_794xN.2003836467_jj80.jpg",
-  },
-  {
-    titre: "Ocarina of Time",
-    annee: 1998,
-    description:
-      "L'un des jeux les plus populaires de la série, Ocarina of Time permet aux joueurs de voyager entre le passé et le présent pour empêcher Ganondorf de conquérir Hyrule.",
-    console: "Nintendo 64",
-    photo:
-      "https://i.etsystatic.com/28082222/r/il/6ce72d/4556964057/il_794xN.4556964057_7i0j.jpg",
-  },
-  {
-    titre: "Majora's Mask",
-    annee: 2000,
-    description:
-      "Link doit sauver le monde de Termina en trois jours dans ce jeu où le temps est une mécanique importante.",
-    console: "Nintendo 64",
-    photo:
-      "https://i.etsystatic.com/28082222/r/il/bb0365/4233483347/il_794xN.4233483347_a96s.jpg",
-  },
-  {
-    titre: "The Wind Waker",
-    annee: 2002,
-    description:
-      "Link explore le Grand Océan dans ce jeu à la direction artistique unique, pour sauver sa sœur Aryll et empêcher Ganondorf de détruire Hyrule.",
-    console: "GameCube",
-    photo:
-      "https://fr.shopping.rakuten.com/photo/The-Legend-Of-Zelda-The-Wind-Waker-Edition-Simple-Jeu-Gamecube-1039090386_ML.jpg",
-  },
-  {
-    titre: "Four Swords Adventures",
-    annee: 2004,
-    description:
-      "Une version améliorée de Four Swords, où quatre joueurs peuvent jouer ensemble en utilisant la Game Boy Advance pour connecter la GameCube.",
-    console: "GameCube",
-    photo:
-      "https://i.ebayimg.com/images/g/z14AAOSw~8dkCwyb/s-l500.jpg",
-  },
-  {
-    titre: "The Minish Cap",
-    annee: 2004,
-    description:
-      "Link explore le monde des Minish dans ce jeu portable où il peut rétrécir à la taille d'un insecte pour résoudre des énigmes.",
-    console: "Game Boy Advance",
-    photo:
-      "https://www.cdiscount.com/pdt2/0/2/6/1/550x550/nin3701519701026/rw/zelda-the-minish-cap-nintendo-game-boy-advance-g.jpg",
-  },
-  {
-    titre: "Twilight Princess",
-    annee: 2006,
-    description:
-      "Link se transforme en loup-garou dans ce jeu sombre et mature, où il doit sauver Hyrule de l'obscurité.",
-    console: "Wii/GameCube",
-    photo:
-      "https://static.fnac-static.com/multimedia/images_produits/ZoomPE/9/0/4/0045496362409/tsp20221202082612/The-Legend-of-Zelda-Twilight-Prince.jpg",
-  },
-  {
-    titre: "Phantom Hourglass",
-    annee: 2007,
-    description:
-      "Une suite de Wind Waker où Link explore des donjons sur un bateau à vapeur avec le personnage de Linebeck.",
-    console: "Nintendo DS",
-    photo:
-      "https://fr.shopping.rakuten.com/photo/the-legend-of-zelda-phantom-hourglass-jeu-nintendo-ds-937651647_ML.jpg",
-  },
-  {
-    titre: "Spirit Tracks",
-    annee: 2009,
-    description:
-      "Link explore Hyrule avec un train à vapeur dans ce jeu où il doit aider la princesse Zelda à restaurer le pouvoir des esprits.",
-    console: "Nintendo DS",
-    photo:
-      "https://www.cdiscount.com/pdt2/4/7/4/1/550x550/0045496469474/rw/the-legend-of-zelda-spirit-tracks-ds-2ds-3ds.jpg",
-  },
-  {
-    titre: "Skyward Sword",
-    annee: 2011,
-    description:
-      "Link doit explorer les cieux pour sauver la princesse Zelda et vaincre le mal ancien connu sous le nom de Démon.",
-    console: "Wii",
-    photo:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Legend_of_Zelda_Skyward_Sword_boxart.png/220px-Legend_of_Zelda_Skyward_Sword_boxart.png",
-  },
-  {
-    titre: "A Link Between Worlds",
-    annee: 2013,
-    description:
-      "Une suite de A Link to the Past, où Link doit sauver Hyrule et Lorule en voyageant entre les deux mondes.",
-    console: "Nintendo 3DS",
-    photo:
-      "https://m.media-amazon.com/images/I/71Td6xdxSOL._AC_SX679_.jpg",
-  },
-  {
-    titre: "Tri Force Heroes",
-    annee: 2015,
-    description:
-      "Un jeu multijoueur où trois joueurs contrôlent chacun un Link et travaillent ensemble pour sauver la princesse Styla.",
-    console: "Nintendo 3DS",
-    photo:
-      "https://fr.shopping.rakuten.com/photo/the-legend-of-zelda-tri-force-heroes-1041548986_ML.jpg",
-  },
-  {
-    titre: "Breath of the Wild",
-    annee: 2017,
-    description:
-      "Un jeu en monde ouvert où Link doit vaincre le Calamité Ganon et sauver le royaume d'Hyrule.",
-    console: "Wii U, Nintendo Switch",
-    photo:
-      "https://m.media-amazon.com/images/I/91jvZUxquKL._AC_SY741_.jpg",
-  },
-  {
-    titre: "Link's Awakening (Remake)",
-    annee: 2019,
-    description:
-      "Un remake du jeu original de Game Boy, où Link doit récupérer les instruments pour réveiller le poisson-rêve.",
-    console: "Nintendo Switch",
-    photo:
-      "https://static.fnac-static.com/multimedia/Images/FR/NR/85/5b/9b/10181509/1540-1/tsp20230112091616/The-Legend-of-Zelda-Link-s-Awakening-Nintendo-Switch.jpg",
-  },
-  {
-    titre: "Hyrule Warriors",
-    annee: 2014,
-    description:
-      "Hyrule Warriors est un beat'em all sur Wii U. Le jeu transpose l'univers de la série Zelda dans un jeu de combat de masse à la Dynasty Warriors qui multiplie les références aux différents jeux de la saga. En marge du mode Légende scénarisé, un mode Aventure s'inspire de la map du premier Zelda pour proposer une multitude de défis teintés d'exploration.",
-    console: "Wii U",
-    photo:
-      "https://m.media-amazon.com/images/I/81EIqT3ARAL._AC_SY741_.jpg",
-  },
-  {
-    titre: "Hyrule Warriors: L'Ère du Fléau",
-    annee: 2020,
-    description:
-      "Un jeu de type hack and slash qui se déroule 100 ans avant les événements de Breath of the Wild, où les joueurs contrôlent différents personnages pour combattre l'armée de Ganon.",
-    console: "Nintendo Switch",
-    photo:
-      "https://m.media-amazon.com/images/I/81vJZPhsHtL._AC_SY741_.jpg",
-  },
+require("dotenv").config();
+const { requestDB } = require('../databasepg')
+const { Client } = require("pg");
+// je définie le MDP grace a .env
+const PG_PASSWORD = process.env.PG_PASSWORD;
+
+
+// const jeuxZelda = [
+//   {
+//     titre: "The Legend of Zelda",
+//     annee: 1986,
+//     description:
+//       "Le premier jeu de la série Zelda, où les joueurs contrôlent Link pour sauver la princesse Zelda et vaincre le maléfique Ganon.",
+//     console: "NES",
+//     photo:
+//       "https://upload.wikimedia.org/wikipedia/en/4/41/Legend_of_zelda_cover_%28with_cartridge%29_gold.png",
+//   },
+//   {
+//     titre: "Zelda II: The Adventure of Link",
+//     annee: 1987,
+//     description:
+//       "Un jeu de rôle d'action dans lequel Link doit trouver les trois reliques pour réveiller la princesse Zelda.",
+//     console: "NES",
+//     photo:
+//       "https://fr.shopping.rakuten.com/photo/Zelda-Ii-The-Adventure-Of-Link-Jeu-Nintendo-Nes-957925970_L.jpg",
+//   },
+//   {
+//     titre: "A Link to the Past",
+//     annee: 1991,
+//     description:
+//       "Considéré comme l'un des meilleurs jeux de la série, A Link to the Past permet aux joueurs de voyager entre le monde de la lumière et le monde des ténèbres pour sauver Hyrule.",
+//     console: "SNES",
+//     photo:
+//       "https://image.jeuxvideo.com/images/sn/l/o/lozpsn0f.jpg",
+//   },
+//   {
+//     titre: "Link's Awakening",
+//     annee: 1993,
+//     description:
+//       "Le premier jeu portable de la série Zelda, où Link doit récupérer les instruments pour réveiller le poisson-rêve.",
+//     console: "Game Boy",
+//     photo:
+//       "https://i.etsystatic.com/16695346/r/il/92514e/2003836467/il_794xN.2003836467_jj80.jpg",
+//   },
+//   {
+//     titre: "Ocarina of Time",
+//     annee: 1998,
+//     description:
+//       "L'un des jeux les plus populaires de la série, Ocarina of Time permet aux joueurs de voyager entre le passé et le présent pour empêcher Ganondorf de conquérir Hyrule.",
+//     console: "Nintendo 64",
+//     photo:
+//       "https://i.etsystatic.com/28082222/r/il/6ce72d/4556964057/il_794xN.4556964057_7i0j.jpg",
+//   },
+//   {
+//     titre: "Majora's Mask",
+//     annee: 2000,
+//     description:
+//       "Link doit sauver le monde de Termina en trois jours dans ce jeu où le temps est une mécanique importante.",
+//     console: "Nintendo 64",
+//     photo:
+//       "https://i.etsystatic.com/28082222/r/il/bb0365/4233483347/il_794xN.4233483347_a96s.jpg",
+//   },
+//   {
+//     titre: "The Wind Waker",
+//     annee: 2002,
+//     description:
+//       "Link explore le Grand Océan dans ce jeu à la direction artistique unique, pour sauver sa sœur Aryll et empêcher Ganondorf de détruire Hyrule.",
+//     console: "GameCube",
+//     photo:
+//       "https://fr.shopping.rakuten.com/photo/The-Legend-Of-Zelda-The-Wind-Waker-Edition-Simple-Jeu-Gamecube-1039090386_ML.jpg",
+//   },
+//   {
+//     titre: "Four Swords Adventures",
+//     annee: 2004,
+//     description:
+//       "Une version améliorée de Four Swords, où quatre joueurs peuvent jouer ensemble en utilisant la Game Boy Advance pour connecter la GameCube.",
+//     console: "GameCube",
+//     photo:
+//       "https://i.ebayimg.com/images/g/z14AAOSw~8dkCwyb/s-l500.jpg",
+//   },
+//   {
+//     titre: "The Minish Cap",
+//     annee: 2004,
+//     description:
+//       "Link explore le monde des Minish dans ce jeu portable où il peut rétrécir à la taille d'un insecte pour résoudre des énigmes.",
+//     console: "Game Boy Advance",
+//     photo:
+//       "https://www.cdiscount.com/pdt2/0/2/6/1/550x550/nin3701519701026/rw/zelda-the-minish-cap-nintendo-game-boy-advance-g.jpg",
+//   },
+//   {
+//     titre: "Twilight Princess",
+//     annee: 2006,
+//     description:
+//       "Link se transforme en loup-garou dans ce jeu sombre et mature, où il doit sauver Hyrule de l'obscurité.",
+//     console: "Wii/GameCube",
+//     photo:
+//       "https://static.fnac-static.com/multimedia/images_produits/ZoomPE/9/0/4/0045496362409/tsp20221202082612/The-Legend-of-Zelda-Twilight-Prince.jpg",
+//   },
+//   {
+//     titre: "Phantom Hourglass",
+//     annee: 2007,
+//     description:
+//       "Une suite de Wind Waker où Link explore des donjons sur un bateau à vapeur avec le personnage de Linebeck.",
+//     console: "Nintendo DS",
+//     photo:
+//       "https://fr.shopping.rakuten.com/photo/the-legend-of-zelda-phantom-hourglass-jeu-nintendo-ds-937651647_ML.jpg",
+//   },
+//   {
+//     titre: "Spirit Tracks",
+//     annee: 2009,
+//     description:
+//       "Link explore Hyrule avec un train à vapeur dans ce jeu où il doit aider la princesse Zelda à restaurer le pouvoir des esprits.",
+//     console: "Nintendo DS",
+//     photo:
+//       "https://www.cdiscount.com/pdt2/4/7/4/1/550x550/0045496469474/rw/the-legend-of-zelda-spirit-tracks-ds-2ds-3ds.jpg",
+//   },
+//   {
+//     titre: "Skyward Sword",
+//     annee: 2011,
+//     description:
+//       "Link doit explorer les cieux pour sauver la princesse Zelda et vaincre le mal ancien connu sous le nom de Démon.",
+//     console: "Wii",
+//     photo:
+//       "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Legend_of_Zelda_Skyward_Sword_boxart.png/220px-Legend_of_Zelda_Skyward_Sword_boxart.png",
+//   },
+//   {
+//     titre: "A Link Between Worlds",
+//     annee: 2013,
+//     description:
+//       "Une suite de A Link to the Past, où Link doit sauver Hyrule et Lorule en voyageant entre les deux mondes.",
+//     console: "Nintendo 3DS",
+//     photo:
+//       "https://m.media-amazon.com/images/I/71Td6xdxSOL._AC_SX679_.jpg",
+//   },
+//   {
+//     titre: "Tri Force Heroes",
+//     annee: 2015,
+//     description:
+//       "Un jeu multijoueur où trois joueurs contrôlent chacun un Link et travaillent ensemble pour sauver la princesse Styla.",
+//     console: "Nintendo 3DS",
+//     photo:
+//       "https://fr.shopping.rakuten.com/photo/the-legend-of-zelda-tri-force-heroes-1041548986_ML.jpg",
+//   },
+//   {
+//     titre: "Breath of the Wild",
+//     annee: 2017,
+//     description:
+//       "Un jeu en monde ouvert où Link doit vaincre le Calamité Ganon et sauver le royaume d'Hyrule.",
+//     console: "Wii U, Nintendo Switch",
+//     photo:
+//       "https://m.media-amazon.com/images/I/91jvZUxquKL._AC_SY741_.jpg",
+//   },
+//   {
+//     titre: "Link's Awakening (Remake)",
+//     annee: 2019,
+//     description:
+//       "Un remake du jeu original de Game Boy, où Link doit récupérer les instruments pour réveiller le poisson-rêve.",
+//     console: "Nintendo Switch",
+//     photo:
+//       "https://static.fnac-static.com/multimedia/Images/FR/NR/85/5b/9b/10181509/1540-1/tsp20230112091616/The-Legend-of-Zelda-Link-s-Awakening-Nintendo-Switch.jpg",
+//   },
+//   {
+//     titre: "Hyrule Warriors",
+//     annee: 2014,
+//     description:
+//       "Hyrule Warriors est un beat'em all sur Wii U. Le jeu transpose l'univers de la série Zelda dans un jeu de combat de masse à la Dynasty Warriors qui multiplie les références aux différents jeux de la saga. En marge du mode Légende scénarisé, un mode Aventure s'inspire de la map du premier Zelda pour proposer une multitude de défis teintés d'exploration.",
+//     console: "Wii U",
+//     photo:
+//       "https://m.media-amazon.com/images/I/81EIqT3ARAL._AC_SY741_.jpg",
+//   },
+//   {
+//     titre: "Hyrule Warriors: L'Ère du Fléau",
+//     annee: 2020,
+//     description:
+//       "Un jeu de type hack and slash qui se déroule 100 ans avant les événements de Breath of the Wild, où les joueurs contrôlent différents personnages pour combattre l'armée de Ganon.",
+//     console: "Nintendo Switch",
+//     photo:
+//       "https://m.media-amazon.com/images/I/81vJZPhsHtL._AC_SY741_.jpg",
+//   },
   
   
-];
+// ];
 
 // function setArray() {
 //   let startButton = document.querySelector(".launch-array");
@@ -210,39 +217,52 @@ const jeuxZelda = [
 // }
 // setArray();
 
-function search() {
+
+// je définie la connexion a la BDD
+
+const db = new Client({
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: PG_PASSWORD,
+  database: "jeux_zelda",
+});
+
+ async function search() {
   const startButton = document.querySelector(".search-button");
   startButton.addEventListener("click", () => {
     let buttonValue = document.querySelector("input");
     let searchValue = buttonValue.value;
     const table = document.querySelector(".target-array");
+    requestDB("SELECT * FROM jeux_zelda");
+    console.log(request);
 
     while (table.rows.length > 0) {
       table.deleteRow(0);
     }
-    for (let i = 0; i < jeuxZelda.length; i++) {
+    for (let i = 0; i < request.length; i++) {
       if (
-        jeuxZelda[i].titre.toLowerCase().includes(searchValue.toLowerCase())
+        request[i].titre.toLowerCase().includes(searchValue.toLowerCase())
       ) {
         const row = table.insertRow(-1);
 
         const cellTitre = row.insertCell(0);
-        cellTitre.innerHTML = jeuxZelda[i].titre;
+        cellTitre.innerHTML = request[i].titre;
 
         const cellAnnee = row.insertCell(1);
-        cellAnnee.innerHTML = jeuxZelda[i].annee;
+        cellAnnee.innerHTML = request[i].annee;
 
         const cellConsole = row.insertCell(2);
-        cellConsole.innerHTML = jeuxZelda[i].console;
+        cellConsole.innerHTML = request[i].console;
 
         const cellDesc = row.insertCell(3);
         cellDesc.id = "cell-desc";
-        cellDesc.innerHTML = jeuxZelda[i].description;
+        cellDesc.innerHTML = request[i].extrait;
 
         const cellImg = row.insertCell(4);
         const img = document.createElement("img");
         img.className = "img-article";
-        img.src = jeuxZelda[i].photo;
+        img.src = request[i].photo;
         cellImg.appendChild(img);
       }
     }
