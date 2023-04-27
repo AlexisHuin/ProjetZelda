12420ce2659ace4fd98c64c218a7834c614e57cc
AlexisHuin <redemption41@gmail.com>
2023-04-27 16:18:46 +0200

    design général

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ const jeuxZelda = [
     titre: "The Legend of Zelda (NES)",
     annee: 1986,
     description:
-      "Le premier jeu de la série Zelda, où les joueurs contrôlent Link pour sauver la princesse Zelda et vaincre le maléfique Ganon.",
+      "Le premier jeu de la sErie Zelda, oU les joueurs contrOlent Link pour sauver la princesse Zelda et vaincre le malEfique Ganon.",
 
     photo:
       "https://upload.wikimedia.org/wikipedia/en/4/41/Legend_of_zelda_cover_%28with_cartridge%29_gold.png",
@@ -12,7 +12,7 @@ const jeuxZelda = [
     titre: "Zelda II: The Adventure of Link (NES)",
     annee: 1987,
     description:
-      "Un jeu de rôle d'action dans lequel Link doit trouver les trois reliques pour réveiller la princesse Zelda.",
+      "Un jeu de rOle d'action dans lequel Link doit trouver les trois reliques pour rEveiller la princesse Zelda.",
 
     photo:
       "https://fr.shopping.rakuten.com/photo/Zelda-Ii-The-Adventure-Of-Link-Jeu-Nintendo-Nes-957925970_L.jpg",
@@ -21,7 +21,7 @@ const jeuxZelda = [
     titre: "A Link to the Past (SNES)",
     annee: 1991,
     description:
-      "Considéré comme l'un des meilleurs jeux de la série, A Link to the Past permet aux joueurs de voyager entre le monde de la lumière et le monde des ténèbres pour sauver Hyrule.",
+      "ConsidErE comme l'un des meilleurs jeux de la sErie, A Link to the Past permet aux joueurs de voyager entre le monde de la lumiEre et le monde des tEnEbres pour sauver Hyrule.",
 
     photo: "https://image.jeuxvideo.com/images/sn/l/o/lozpsn0f.jpg",
   },
@@ -29,7 +29,7 @@ const jeuxZelda = [
     titre: "Link's Awakening (Game Boy)",
     annee: 1993,
     description:
-      "Le premier jeu portable de la série Zelda, où Link doit récupérer les instruments pour réveiller le poisson-rêve.",
+      "Le premier jeu portable de la sErie Zelda, oU Link doit rEcupErer les instruments pour rEveiller le poisson-rEve.",
 
     photo:
       "https://i.etsystatic.com/16695346/r/il/92514e/2003836467/il_794xN.2003836467_jj80.jpg",
@@ -38,7 +38,7 @@ const jeuxZelda = [
     titre: "Ocarina of Time (N64)",
     annee: 1998,
     description:
-      "L'un des jeux les plus populaires de la série, Ocarina of Time permet aux joueurs de voyager entre le passé et le présent pour empêcher Ganondorf de conquérir Hyrule.",
+      "L'un des jeux les plus populaires de la sErie, Ocarina of Time permet aux joueurs de voyager entre le passE et le prEsent pour empEcher Ganondorf de conquErir Hyrule.",
 
     photo:
       "https://i.etsystatic.com/28082222/r/il/6ce72d/4556964057/il_794xN.4556964057_7i0j.jpg",
@@ -47,7 +47,7 @@ const jeuxZelda = [
     titre: "Majora's Mask (N64)",
     annee: 2000,
     description:
-      "Link doit sauver le monde de Termina en trois jours dans ce jeu où le temps est une mécanique importante.",
+      "Link doit sauver le monde de Termina en trois jours dans ce jeu oU le temps est une mEcanique importante.",
 
     photo:
       "https://i.etsystatic.com/28082222/r/il/bb0365/4233483347/il_794xN.4233483347_a96s.jpg",
@@ -56,7 +56,7 @@ const jeuxZelda = [
     titre: "The Wind Waker (GC)",
     annee: 2002,
     description:
-      "Link explore le Grand Océan dans ce jeu à la direction artistique unique, pour sauver sa sœur Aryll et empêcher Ganondorf de détruire Hyrule.",
+      "Link explore le Grand OcEan dans ce jeu à la direction artistique unique, pour sauver sa sœur Aryll et empEcher Ganondorf de dEtruire Hyrule.",
 
     photo:
       "https://fr.shopping.rakuten.com/photo/The-Legend-Of-Zelda-The-Wind-Waker-Edition-Simple-Jeu-Gamecube-1039090386_ML.jpg",
@@ -65,7 +65,7 @@ const jeuxZelda = [
     titre: "Four Swords Adventures (GC)",
     annee: 2004,
     description:
-      "Une version améliorée de Four Swords, où quatre joueurs peuvent jouer ensemble en utilisant la Game Boy Advance pour connecter la GameCube.",
+      "Une version amEliorEe de Four Swords, oU quatre joueurs peuvent jouer ensemble en utilisant la Game Boy Advance pour connecter la GameCube.",
 
     photo: "https://i.ebayimg.com/images/g/z14AAOSw~8dkCwyb/s-l500.jpg",
   },
@@ -73,7 +73,7 @@ const jeuxZelda = [
     titre: "The Minish Cap (Advance)",
     annee: 2004,
     description:
-      "Link explore le monde des Minish dans ce jeu portable où il peut rétrécir à la taille d'un insecte pour résoudre des énigmes.",
+      "Link explore le monde des Minish dans ce jeu portable oU il peut rEtrEcir à la taille d'un insecte pour rEsoudre des Enigmes.",
 
     photo:
       "https://www.cdiscount.com/pdt2/0/2/6/1/550x550/nin3701519701026/rw/zelda-the-minish-cap-nintendo-game-boy-advance-g.jpg",
@@ -82,7 +82,7 @@ const jeuxZelda = [
     titre: "Twilight Princess (GC/Wii)",
     annee: 2006,
     description:
-      "Link se transforme en loup-garou dans ce jeu sombre et mature, où il doit sauver Hyrule de l'obscurité.",
+      "Link se transforme en loup-garou dans ce jeu sombre et mature, oU il doit sauver Hyrule de l'obscuritE.",
 
     photo:
       "https://static.fnac-static.com/multimedia/images_produits/ZoomPE/9/0/4/0045496362409/tsp20221202082612/The-Legend-of-Zelda-Twilight-Prince.jpg",
@@ -91,7 +91,7 @@ const jeuxZelda = [
     titre: "Phantom Hourglass (DS)",
     annee: 2007,
     description:
-      "Une suite de Wind Waker où Link explore des donjons sur un bateau à vapeur avec le personnage de Linebeck.",
+      "Une suite de Wind Waker oU Link explore des donjons sur un bateau à vapeur avec le personnage de Linebeck.",
 
     photo:
       "https://fr.shopping.rakuten.com/photo/the-legend-of-zelda-phantom-hourglass-jeu-nintendo-ds-937651647_ML.jpg",
@@ -100,7 +100,7 @@ const jeuxZelda = [
     titre: "Spirit Tracks (DS)",
     annee: 2009,
     description:
-      "Link explore Hyrule avec un train à vapeur dans ce jeu où il doit aider la princesse Zelda à restaurer le pouvoir des esprits.",
+      "Link explore Hyrule avec un train à vapeur dans ce jeu oU il doit aider la princesse Zelda à restaurer le pouvoir des esprits.",
 
     photo:
       "https://www.cdiscount.com/pdt2/4/7/4/1/550x550/0045496469474/rw/the-legend-of-zelda-spirit-tracks-ds-2ds-3ds.jpg",
@@ -109,7 +109,7 @@ const jeuxZelda = [
     titre: "Skyward Sword (Wii/Switch)",
     annee: 2011,
     description:
-      "Link doit explorer les cieux pour sauver la princesse Zelda et vaincre le mal ancien connu sous le nom de Démon.",
+      "Link doit explorer les cieux pour sauver la princesse Zelda et vaincre le mal ancien connu sous le nom de DEmon.",
 
     photo:
       "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Legend_of_Zelda_Skyward_Sword_boxart.png/220px-Legend_of_Zelda_Skyward_Sword_boxart.png",
@@ -118,7 +118,7 @@ const jeuxZelda = [
     titre: "A Link Between Worlds (3DS)",
     annee: 2013,
     description:
-      "Une suite de A Link to the Past, où Link doit sauver Hyrule et Lorule en voyageant entre les deux mondes.",
+      "Une suite de A Link to the Past, oU Link doit sauver Hyrule et Lorule en voyageant entre les deux mondes.",
 
     photo: "https://m.media-amazon.com/images/I/71Td6xdxSOL._AC_SX679_.jpg",
   },
@@ -126,7 +126,7 @@ const jeuxZelda = [
     titre: "Tri Force Heroes (3DS)",
     annee: 2015,
     description:
-      "Un jeu multijoueur où trois joueurs contrôlent chacun un Link et travaillent ensemble pour sauver la princesse Styla.",
+      "Un jeu multijoueur oU trois joueurs contrOlent chacun un Link et travaillent ensemble pour sauver la princesse Styla.",
 
     photo:
       "https://fr.shopping.rakuten.com/photo/the-legend-of-zelda-tri-force-heroes-1041548986_ML.jpg",
@@ -135,7 +135,7 @@ const jeuxZelda = [
     titre: "Breath of the Wild (Switch)",
     annee: 2017,
     description:
-      "Un jeu en monde ouvert où Link doit vaincre le Calamité Ganon et sauver le royaume d'Hyrule.",
+      "Un jeu en monde ouvert oU Link doit vaincre le CalamitE Ganon et sauver le royaume d'Hyrule.",
 
     photo: "https://m.media-amazon.com/images/I/91jvZUxquKL._AC_SY741_.jpg",
   },
@@ -143,7 +143,7 @@ const jeuxZelda = [
     titre: "Link's Awakening (Remake) (Switch)",
     annee: 2019,
     description:
-      "Un remake du jeu original de Game Boy, où Link doit récupérer les instruments pour réveiller le poisson-rêve.",
+      "Un remake du jeu original de Game Boy, oU Link doit rEcupErer les instruments pour rEveiller le poisson-rEve.",
 
     photo:
       "https://static.fnac-static.com/multimedia/Images/FR/NR/85/5b/9b/10181509/1540-1/tsp20230112091616/The-Legend-of-Zelda-Link-s-Awakening-Nintendo-Switch.jpg",
@@ -152,15 +152,15 @@ const jeuxZelda = [
     titre: "Hyrule Warriors (Wii U)",
     annee: 2014,
     description:
-      "Hyrule Warriors est un beat'em all sur Wii U. Le jeu transpose l'univers de la série Zelda dans un jeu de combat de masse à la Dynasty Warriors qui multiplie les références aux différents jeux de la saga. En marge du mode Légende scénarisé, un mode Aventure s'inspire de la map du premier Zelda pour proposer une multitude de défis teintés d'exploration.",
+      "Hyrule Warriors est un beat'em all sur Wii U. Le jeu transpose l'univers de la sErie Zelda dans un jeu de combat de masse à la Dynasty Warriors qui multiplie les rEfErences aux diffErents jeux de la saga. En marge du mode LEgende scEnarisE, un mode Aventure s'inspire de la map du premier Zelda pour proposer une multitude de dEfis teintEs d'exploration.",
 
     photo: "https://m.media-amazon.com/images/I/81EIqT3ARAL._AC_SY741_.jpg",
   },
   {
-    titre: "Hyrule Warriors: L'Ère du Fléau (Switch)",
+    titre: "Hyrule Warriors: L'Ere du FlEau (Switch)",
     annee: 2020,
     description:
-      "Un jeu de type hack and slash qui se déroule 100 ans avant les événements de Breath of the Wild, où les joueurs contrôlent différents personnages pour combattre l'armée de Ganon.",
+      "Un jeu de type hack and slash qui se dEroule 100 ans avant les EvEnements de Breath of the Wild, oU les joueurs contrOlent diffErents personnages pour combattre l'armEe de Ganon.",
 
     photo: "https://m.media-amazon.com/images/I/81vJZPhsHtL._AC_SY741_.jpg",
   },
@@ -168,7 +168,7 @@ const jeuxZelda = [
     titre: "Tears Of The Kingdom (Switch)",
     annee: 2023,
     description:
-      "Le dernier Zelda, beaucoup de mystères autour de cet épisode, qui va mêler exploration et construction d'arme et de véhicules !",
+      "Le dernier Zelda, beaucoup de mystEres autour de cet Episode, qui va mEler exploration et construction d'arme et de vEhicules !",
 
     photo: "https://media.auchan.fr/A0220230220000184480PRIMARY_1200x1200/B2CD/",
   }
@@ -259,3 +259,8 @@ search();
   });
   /* ma fonction s'auto excecute avec le () */
 })();
+
+document.addEventListener("mousemove", function(e) {
+  document.documentElement.style.setProperty('--x', e.clientX + "px");
+  document.documentElement.style.setProperty('--y', e.clientY + "px");
+});
